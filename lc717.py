d0a3617caa23913232896c240037e3497f712ba6
Miao <hongmiao0118@gmail.com>
2018-10-29 17:14:12 -0400

    leetcode 717 array easy: use while loop, hard to understand the question

---
+++ lc717.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isOneBitCharacter(self, bits):
+        """
+        :type bits: List[int]
+        :rtype: bool
+        """
+        
+        i = 0
+        if len(bits) == 1:
+            return True
+        
+        while i < len(bits) - 1:
+            if bits[i] == 1:
+                i += 2
+            else:
+                i += 1
+        return i == len(bits) - 1
