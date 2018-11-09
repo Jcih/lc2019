755565f80e16983d3266b5cf166a55d4b6b28a0d
Miao <hongmiao0118@gmail.com>
2018-11-08 23:42:57 -0500

    leetcode 189 array easy using slice

---
+++ lc189.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+
+        
+        nums[:k], nums[k:] = nums[len(nums)-k:], nums[:len(nums) - k]
+       
+                
+            
