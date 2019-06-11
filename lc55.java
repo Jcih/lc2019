8f5c116a294fd79aa79dbe2349b37aa64688a290
mhong <mhong@predicine.com>
2019-06-11 00:12:44 -0700

    Jump Game I easy   II hard

---
+++ lc55.java
@@ -0,0 +1,15 @@
+//Jump Game
+class Solution {
+    public boolean canJump(int[] nums) {
+        int farthest = nums[0];
+            for (int i = 0; i < nums.length; i++) {
+                if (i <= farthest && nums[i] + i > farthest) {
+                    //i <= farthest is to make sure that this current i is within the current range
+                    // nums[i]+i > farthest is to make sure that it's necessary to update farthest with current nums[i]+i
+                    farthest = nums[i] + i;
+                }
+            }
+            return farthest >= nums.length - 1;
+        
+    }
+}
