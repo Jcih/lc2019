97edfd406025d0d361eea2711801e31e8b448715
mhong <mhong@predicine.com>
2019-07-21 22:30:37 -0700

    update tree

--- lc144.java
+++ lc144.java
@@ -1,4 +1,4 @@
-//Solution I: faster than 100%
+//Solution I: faster than 100%, Recursive. 
 
 
 /**
@@ -23,4 +23,39 @@ class Solution {
         dfs(res, root.left);
         dfs(res, root.right);
     }
+}
+
+
+// Solution II. Follow up: do it iteratively
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) return res;
+        
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            res.add(curr.val);
+            
+            if (curr.right != null) {
+                stack.push(curr.right);
+            }
+            if (curr.left != null) {
+                stack.push(curr.left);
+            }
+        }
+        
+        return res;
+    }
+    
 }
