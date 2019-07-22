2c28eea30c6c7fd56c883358cc4f512dba88d237
mhong <mhong@predicine.com>
2019-07-21 22:18:01 -0700

    tree preorder

---
+++ lc144.java
@@ -0,0 +1,26 @@
+//Solution I: faster than 100%
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        dfs(res, root);
+        return res;
+    }
+    
+    public void dfs(List<Integer> res, TreeNode root) {
+        if (root == null) return;
+        res.add(root.val);
+        dfs(res, root.left);
+        dfs(res, root.right);
+    }
+}
