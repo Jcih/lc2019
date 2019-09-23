4e5fb985381ad9a96483f80a7897726a7d7cd7ec
mhong <mhong@predicine.com>
2019-09-22 22:44:08 -0700

    leetcode 173 using stack

--- lc98.java
+++ lc98.java
@@ -20,3 +20,37 @@ class Solution {
         return left && right;
     }
 }
+
+
+//Solution II
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root== null || (root.left == null && root.right == null)) {
+            return true;
+        }
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean isValidBST(TreeNode root, long low, long high) {
+        if (root == null) {
+            return true;
+        }
+        
+        boolean isLeftBST = isValidBST(root.left, low, root.val);
+        boolean isRightBST = isValidBST(root.right, root.val, high);
+        
+        boolean isBST = (long)root.val > low && root.val < high;
+            
+        
+        return isLeftBST && isRightBST && isBST;
+    }
+}
